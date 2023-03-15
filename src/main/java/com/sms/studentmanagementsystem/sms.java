1323a3678c9feb88ab11e033a981c027156d3ef7
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2023-03-15 18:16:19 +0500

    add starter files

---
+++ src/main/java/com/sms/studentmanagementsystem/sms.java
@@ -0,0 +1,11 @@
+package com.sms.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sms {
+	public static void main(String[] args) {
+		SpringApplication.run(sms.class, args);
+	}
+}
